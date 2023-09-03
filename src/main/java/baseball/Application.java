56cc49e83ce4422344650b1b7821015af3d8e827
doha <dohaahi@naver.com>
2023-09-03 19:21:13 +0900

    feat: 숫자야구 프로그램 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.service.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        GameService gameService = new GameService();
+
+        GameController gameController = new GameController(inputView, outputView, gameService);
+        gameController.start();
     }
 }
